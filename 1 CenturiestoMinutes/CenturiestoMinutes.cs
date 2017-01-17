92800ba29cd3a6074ab6d6d29c5d15e4e49ecd16
Atanas Nikolov <atanas.nikolov74@gmail.com>
2017-01-17 14:07:52 +0200

    First problem
    Just added first problem

---
+++ 1 CenturiestoMinutes/CenturiestoMinutes.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_CenturiestoMinutes
+{
+    class CenturiestoMinutes
+    {
+        static void Main(string[] args)
+        {
+            int century = int.Parse(Console.ReadLine());
+            int year = century * 100;
+            double day = year * 365.2422;
+            int hours = (int)day * 24;
+            int minute = hours * 60;
+            Console.WriteLine("{0} centuries = {1} years = {2:f0} days = {3} hours = {4} minutes", century, year, day, hours, minute);
+        }
+    }
+}
